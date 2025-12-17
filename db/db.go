ddadd362f4ad531374691e0a43856740144c9aa6
ChunWei202209 <awei.liu.paris202209@gmail.com>
2025-12-17 22:34:17 +0800

    feat: 創造使用者 table

--- db/db.go
+++ db/db.go
@@ -23,6 +23,20 @@ func InitDB() {
 
 // 這是一個初始化用的函式，通常只在程式啟動時呼叫一次
 func createTables() {
+
+	createUsersTable := `
+		CREATE TABLE IF NOT EXISTS users (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			email TEXT NOT NULL UNIQUE,
+			password TEXT NOT NULL
+		)
+	`
+	_, err := DB.Exec(createUsersTable)
+
+	if err != nil {
+		panic("無法創造 users table." + err.Error())
+	}
+
 	createEventsTable := `
 		CREATE TABLE IF NOT EXISTS events (
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -30,10 +44,11 @@ func createTables() {
 			description TEXT NOT NULL,
 			location TEXT NOT NULL,
 			dateTime DATETIME NOT NULL,
-			user_id INTEGER
+			user_id INTEGER,
+			FOREIGN KEY(user_id) REFERENCES users(id)
 		)
 	`
-	_, err := DB.Exec(createEventsTable)
+	_, err = DB.Exec(createEventsTable)
 
 	if err != nil {
 		panic("無法創造 events table." + err.Error())
