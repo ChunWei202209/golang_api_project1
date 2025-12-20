b0649322821ad956b15f82b2b39797ed8669117f
ChunWei202209 <awei.liu.paris202209@gmail.com>
2025-12-20 10:58:46 +0800

    fix: 加入 token 註解

--- utils/jwt.go
+++ utils/jwt.go
@@ -8,6 +8,8 @@ import (
 
 const secretKey = "supersecret"
 
+// 1️⃣ 建立一張「還沒蓋章的身分證」
+// 回傳 token 還有可能失敗的 error
 func GenerateToken(email string, userId int64) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"email": email,
@@ -15,5 +17,6 @@ func GenerateToken(email string, userId int64) (string, error) {
 		"exp": time.Now().Add(time.Hour *2).Unix(), // 現在時間 + 2 小時
 	})
 
+	// 2️⃣ 用 secret key 蓋章，變成真的 JWT
 	return token.SignedString([]byte(secretKey))
 }
